2072794dd5c3b55f32e0548534d4ce45ce85e3e5
Anish Chapagain <anishchapagain@gmail.com>
2025-06-14 21:44:04 +0545

    num_ctx changes comment

--- hub_agent_a.py
+++ hub_agent_a.py
@@ -14,9 +14,9 @@ def get_weather(city):
 def main():
     print("Hello from huggingface-agents!")
     model = LiteLLMModel(
-        model_id="ollama_chat/gemma3:latest",  # Or try other Ollama-supported models
+        model_id="ollama_chat/gemma3:latest",  # Or try other Ollama-supported models 
         api_base="http://127.0.0.1:11434",  # Default Ollama local server
-        num_ctx=8192,
+        num_ctx=8192, # plan ctx
     )
     print(model)
     print(dir(model))
